dfcdcbafc221b0bb9b2531f03db01bb142d4f12e
samay15jan <samay15jan@gmail.com>
2023-08-12 20:33:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Tasks from './components/Tasks';
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState (false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -45,8 +46,8 @@ function App() {
 
   return (
     <div className="Container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={reminderTask}/> : 'No current tasks set'}
     </div>
   );
