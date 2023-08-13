b62700e68d4cd82c03b4ef39523ad74bcf87d9f4
samay15jan <samay15jan@gmail.com>
2023-08-13 15:32:51 +0530

    added theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
+import Theme from './Theme'
 
-const Header = ({title, onAdd, showAdd}) => {
-
+const Header = ({onAdd, showAdd, changeTheme}) => {
   return (
     <header className='header'>
-      <h1>{title}</h1>
+      <Theme />
       <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
     </header>
   )
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,9 @@
-import { FaTimes } from "react-icons/fa";
 
 const Task = ({task, onDelete, onToggle}) => {
   return (
     <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
       <h3>
-        {task.text} <FaTimes style={{color:'red', cursor:'pointer'}} onClick={() => onDelete(task.id)}/>
+        {task.text} <button style={{color:'red', cursor:'pointer'}} onClick={() => onDelete(task.id)}/>
       </h3>
       <p>{task.day}</p>
     </div>
