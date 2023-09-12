df4198e19a8692f6f89f215e035ab2b1733e5042
Samay Kumar <samay15jan@gmail.com>
2023-09-13 00:51:48 +0530

    Vanilla Tilt Added !!

--- src/components/main/Task.js
+++ src/components/main/Task.js
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useEffect, useRef } from "react";
 import { AiOutlineDelete } from "react-icons/ai"
 import { BsBell, BsBellSlash } from "react-icons/bs"
+import VanillaTilt from "vanilla-tilt";
+
 
 const Task = ({task, onDelete, onToggle}) => {
+  const tiltRef = useRef(null);
+  useEffect(() => {
+    if(tiltRef.current) {
+      VanillaTilt.init(tiltRef.current,{
+      max: 5,
+      speed: 200,
+      glare: true,
+      'max-glare': 0.1,
+    });
+  }},[]);
   return (
-    <div className='task'>
+    <div className='task'  ref={tiltRef}>
       <div className="flex">
         {task.text}
       </div>
@@ -17,7 +29,7 @@ const Task = ({task, onDelete, onToggle}) => {
           </div>
         </button>
         <div className="w-56 pt-1">
-        {task.imageUrl && <img className="rounded-xl" src={task.imageUrl}/>}
+        {task.imageUrl && <img className="rounded-xl " ref={tiltRef} src={task.imageUrl}/>}
       </div>
         <button style={{color:'rgb(224, 125, 142)', cursor:'pointer'}} onClick={() => onToggle(task.id, task.taskLocation)}>
           <div className="text-xl">
